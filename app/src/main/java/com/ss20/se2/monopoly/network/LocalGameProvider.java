eb9b7abb1222adcaeb2f3286d117df7fb9ffa03e
Valentin Gavran <valentin.gavran.99@gmail.com>
2020-04-09 15:29:36 +0200

    Add class, which makes the game findable in the local network
    When other devices use Android Network Service Discovery to search for the service 'monopoly', they can then determine the IP address and port of the device that provides the service and thus a server (the lobby).

---
+++ app/src/main/java/com/ss20/se2/monopoly/network/LocalGameProvider.java
@@ -0,0 +1,94 @@
+package com.ss20.se2.monopoly.network;
+
+import android.content.Context;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.util.Log;
+
+public class LocalGameProvider{
+
+	// Singleton instance
+	private static LocalGameProvider instance;
+	// Instance variables
+	private NsdServiceInfo serviceInfo;
+	private NsdManager.RegistrationListener registrationListener;
+	private String serviceName = "monopoly";
+
+	/**
+	 * Use the getInstance() method to get the instance of this class
+	 */
+	private LocalGameProvider(){
+		this.serviceInfo = createServiceInfo(serviceName);
+		initializeRegistrationListener();
+	}
+
+	public static LocalGameProvider getInstance(){
+		if (instance == null){
+			instance = new LocalGameProvider();
+		}
+		return instance;
+	}
+
+	private NsdServiceInfo createServiceInfo(String serviceName){
+		// Create the NsdServiceInfo object, and populate it.
+		NsdServiceInfo serviceInfo = new NsdServiceInfo();
+		// The name is subject to change based on conflicts
+		// with other services advertised on the same network.
+		serviceInfo.setServiceName(serviceName);
+		serviceInfo.setServiceType(NetworkUtilities.NSD_SERVICE_TYPE);
+		return serviceInfo;
+	}
+
+	private void initializeRegistrationListener(){
+		registrationListener = new NsdManager.RegistrationListener(){
+			@Override
+			public void onServiceRegistered(NsdServiceInfo NsdServiceInfo){
+				// Save the service name. Android may have changed it in order to
+				// resolve a conflict, so update the name you initially requested
+				// with the name Android actually used.
+				serviceName = NsdServiceInfo.getServiceName();
+			}
+
+			@Override
+			public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode){
+				// Registration failed! Put debugging code here to determine why.
+				Log.d("LocalGameProvider", "onRegistrationFailed: " + errorCode);
+			}
+
+			@Override
+			public void onServiceUnregistered(NsdServiceInfo arg0){
+				// Service has been unregistered. This only happens when you call
+				// NsdManager.unregisterService() and pass in this listener.
+				Log.i("LocalGameProvider", "onServiceUnregistered: Successfully unregistered");
+			}
+
+			@Override
+			public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode){
+				// Unregistration failed. Put debugging code here to determine why.
+				Log.e("LocalGameProvider", "onUnregistrationFailed: " + errorCode);
+			}
+		};
+	}
+
+	/**
+	 * Makes the game visible in the local network. Other users can then discover the IP address of the server
+	 */
+	public void showGameInNetwork(Context context, int port){
+		NsdManager manager = (NsdManager) context.getSystemService(Context.NSD_SERVICE);
+		// Possible that the server has been terminated and has been assigned a new port
+		serviceInfo.setPort(port);
+		// Publish this service to all people on the network who are looking for a service
+		manager.registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener);
+	}
+
+	/**
+	 * Makes the game invisible on the local network. But if somehow the IP address is known, the server can still be accessed.
+	 * Call this after starting the game and leaving the lobby.
+	 * Needed for cleanup
+	 */
+	public void hideGameInNetwork(Context context){
+		NsdManager manager = (NsdManager) context.getSystemService(Context.NSD_SERVICE);
+		// Make the service no longer available to people in the network who are looking for services
+		manager.unregisterService(registrationListener);
+	}
+}
--- app/src/main/java/com/ss20/se2/monopoly/network/NetworkUtilities.java
+++ app/src/main/java/com/ss20/se2/monopoly/network/NetworkUtilities.java
@@ -1,9 +1,6 @@
 package com.ss20.se2.monopoly.network;
 
-public class NetworkUtilities{
+class NetworkUtilities{
 
-	public static final String NSD_SERVICE_TYPE = "_http._tcp";
-	public static final String NSD_SERVICE_NAME = "monopoly";
-	public static final String TAG = "MonopolyApp";
-	public static final int MAX_PLAYERS = 8;
+	static final String NSD_SERVICE_TYPE = "_http._tcp";
 }
