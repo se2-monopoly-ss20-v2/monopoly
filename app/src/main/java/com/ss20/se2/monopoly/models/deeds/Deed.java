c3b9bef5464ac72e6a94593678c7f0204dc6901d
Hannes Dermutz <hadermutz@edu.aau.at>
2020-04-18 20:36:08 +0200

    Add Description for Deed

--- app/src/main/java/com/ss20/se2/monopoly/models/deeds/Deed.java
+++ app/src/main/java/com/ss20/se2/monopoly/models/deeds/Deed.java
@@ -1,5 +1,12 @@
 package com.ss20.se2.monopoly.models.deeds;
-
+/*
+ * Deed definition:
+ * A deed for each property is given to a player to signify ownership, and specifies purchase price, mortgage value,
+ * the cost of building houses and hotels on that property, and the various rents depending on how developed the property is.
+ *
+ * When you have the physical version of monopoly this relates to all Cards a Player can buy.
+ * Can also be understood as - (German): Sammelbegriff für Gründstücke, Bahnhöfe, Werke.
+ */
 public abstract class Deed extends GameTile{
 
 	private int price;
