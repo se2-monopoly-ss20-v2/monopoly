ef8f8fc5b5369a7ab89c62ebddab19de44d612db
Chfaeser <62070587+Chfaeser@users.noreply.github.com>
2020-04-19 11:22:11 +0200

    Add Classes and basic structure and functionality
    Added Dice and Gameboard Class
    Implemented basic Dice functionality
    Created base structure for gameboard

---
+++ app/src/main/java/com/ss20/se2/monopoly/models/Gameboard.java
@@ -0,0 +1,14 @@
+package com.ss20.se2.monopoly.models;
+
+public class Gameboard{
+
+	GamePiece[] gameboardArray;
+
+	public Gameboard(){
+		gameboardArray = new GamePiece[40];
+	}
+
+	public void move(String playName, int diceroll){
+		
+	}
+}
