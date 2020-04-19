671d888e704c6a3d9d32ee6d781a8058acac7193
Chfaeser <62070587+Chfaeser@users.noreply.github.com>
2020-04-19 13:53:11 +0200

    Implemented move method
    Implemented basic movement for prototype

--- app/src/main/java/com/ss20/se2/monopoly/models/Gameboard.java
+++ app/src/main/java/com/ss20/se2/monopoly/models/Gameboard.java
@@ -8,7 +8,26 @@ public class Gameboard{
 		gameboardArray = new GamePiece[40];
 	}
 
-	public void move(String playName, int diceroll){
-		
+	public void move(String name, int diceroll){
+		GamePiece temp = null;
+		int position = 0;
+		for (int i = 0; i < 40; i++){
+			if (gameboardArray[i] != null && name.equals(gameboardArray[i].getName())){
+				position = i;
+				temp = gameboardArray[i];
+				gameboardArray[i] = null;
+			}
+		}
+		if (position + diceroll < 40){
+			if (gameboardArray[position + diceroll] == null){
+				gameboardArray[position + diceroll] = temp;
+				temp = null;
+			}
+		} else {
+			if (gameboardArray[(position + diceroll) - 40] == null){
+				gameboardArray[(position + diceroll) - 40] = temp;
+				temp = null;
+			}
+		}
 	}
 }
