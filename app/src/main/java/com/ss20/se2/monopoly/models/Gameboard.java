f4264b3ec260940241e0f8152a2708d9ecb446a7
Chfaeser <62070587+Chfaeser@users.noreply.github.com>
2020-04-21 14:48:54 +0200

    Updated move Method in Gameboard
    Updated move Method with a loop for the movement on the UI

--- app/src/main/java/com/ss20/se2/monopoly/models/Gameboard.java
+++ app/src/main/java/com/ss20/se2/monopoly/models/Gameboard.java
@@ -2,7 +2,7 @@ package com.ss20.se2.monopoly.models;
 
 public class Gameboard{
 
-	GamePiece[] gameboardArray;
+	public GamePiece[] gameboardArray;
 
 	public Gameboard(){
 		gameboardArray = new GamePiece[40];
@@ -12,6 +12,8 @@ public class Gameboard{
 	public void move(String name, int diceroll){
 		GamePiece temp = null;
 		int position = 0;
+		int ret = 0;
+		
 		for (int i = 0; i < 40; i++){
 			if (gameboardArray[i] != null && name.equals(gameboardArray[i].getName())){
 				position = i;
@@ -28,6 +30,20 @@ public class Gameboard{
 			if (gameboardArray[(position + diceroll) - 40] == null){
 				gameboardArray[(position + diceroll) - 40] = temp;
 				temp = null;
+			}
+		}
+		for (int j = 1; j <= diceroll; j++){
+			if (position + j <= 10){
+				ret = 1;
+			}
+			if (position + j <= 20 && position + j > 10){
+				ret = 2;
+			}
+			if (position + j <= 30 && position + j > 20){
+				ret = 3;
+			}
+			if (position + j <= 40 && position + j > 30){
+				ret = 4;
 			}
 		}
 	}
