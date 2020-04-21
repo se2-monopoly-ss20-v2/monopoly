3fa0d9d992d89830d6c164d7d63f2187f274d031
SgtGraf <Roman-graf2000@gmx.at>
2020-04-21 19:55:09 +0200

    Display Playermovement
    added methods getPosition and moveUI

--- app/src/main/java/com/ss20/se2/monopoly/MainActivity.java
+++ app/src/main/java/com/ss20/se2/monopoly/MainActivity.java
@@ -7,40 +7,81 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import com.ss20.se2.monopoly.models.Dice;
+import com.ss20.se2.monopoly.models.GamePiece;
 import com.ss20.se2.monopoly.models.Gameboard;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-	Button button_rollDice;
-	TextView view_numberDice;
-	TextView view_position;
-	Dice dice = new Dice();
-	Gameboard gameboard = new Gameboard();
-	int amount;
-	int playerPosition = 0;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		button_rollDice = findViewById(R.id.button_roll_dice);
-		view_numberDice = findViewById(R.id.view_number_dice);
-		view_position = findViewById(R.id.view_position);
-
-		button_rollDice.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				amount = dice.roll();
-				view_numberDice.setText(Integer.toString(amount));
-				gameboard.move("Player 1", amount);
-
-				//TODO: Spielerposition darstellen
+    Button button_rollDice;
+    TextView view_numberDice;
+    TextView view_position;
 
 
+    Dice dice = new Dice();
+    Gameboard gameboard = new Gameboard();
 
-			}
-		});
+    int amount;
 
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gameboard.gameboardArray[0] = new GamePiece("Player 1");
+        button_rollDice = findViewById(R.id.button_roll_dice);
+        view_numberDice = findViewById(R.id.view_number_dice);
+        view_position = findViewById(R.id.number_playerposition);
+
+        button_rollDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amount = dice.roll();
+
+                gameboard.move("Player 1", amount);
+
+
+                float originalX = findViewById(R.id.imageView55).getX();
+                float originalY = findViewById(R.id.imageView55).getY();
+
+
+                for (int i = 0; i < amount; i++) {
+
+                    int x = gameboard.moveUI("Player 1", amount, i);
+                    float xaxis = findViewById(R.id.imageView55).getX();
+                    float yaxis = findViewById(R.id.imageView55).getY();
+
+
+                    switch (x) {
+                        case 1: {
+                            findViewById(R.id.imageView55).setX(xaxis - 70);
+                            break;
+                        }
+                        case 2: {
+                            findViewById(R.id.imageView55).setY(yaxis - 70);
+                            break;
+                        }
+                        case 3: {
+                            findViewById(R.id.imageView55).setX(xaxis + 70);
+                            break;
+                        }
+                        case 4: {
+                            findViewById(R.id.imageView55).setY(yaxis + 70);
+                            break;
+                        }
+                        default:
+                            break;
+                    }
+
+                }
+
+                view_numberDice.setText(Integer.toString(amount));
+                view_position.setText(Integer.toString(gameboard.getPosition("Player 1")));
+
+            }
+        });
+
+        //TODO: Spielerposition auf Ursprung zurücksetzen um Verschiebung entgegenzuwirken. -> originalx,y
+
+    }
 }
--- app/src/main/java/com/ss20/se2/monopoly/models/Gameboard.java
+++ app/src/main/java/com/ss20/se2/monopoly/models/Gameboard.java
@@ -1,50 +1,62 @@
 package com.ss20.se2.monopoly.models;
 
-public class Gameboard{
+public class Gameboard {
 
-	public GamePiece[] gameboardArray;
+    public GamePiece[] gameboardArray;
 
-	public Gameboard(){
-		gameboardArray = new GamePiece[40];
-	}
+    public Gameboard() {
+        gameboardArray = new GamePiece[40];
+    }
+
+    public int getPosition(String name) {
+        int position = 0;
+        for (int i = 0; i < 40; i++) {
+            if (gameboardArray[i] != null && name.equals(gameboardArray[i].getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 
-	public void move(String name, int diceroll){
-		GamePiece temp = null;
-		int position = 0;
-		int ret = 0;
-		
-		for (int i = 0; i < 40; i++){
-			if (gameboardArray[i] != null && name.equals(gameboardArray[i].getName())){
-				position = i;
-				temp = gameboardArray[i];
-				gameboardArray[i] = null;
-			}
-		}
-		if (position + diceroll < 40){
-			if (gameboardArray[position + diceroll] == null){
-				gameboardArray[position + diceroll] = temp;
-				temp = null;
-			}
-		} else {
-			if (gameboardArray[(position + diceroll) - 40] == null){
-				gameboardArray[(position + diceroll) - 40] = temp;
-				temp = null;
-			}
-		}
-		for (int j = 1; j <= diceroll; j++){
-			if (position + j <= 10){
-				ret = 1;
-			}
-			if (position + j <= 20 && position + j > 10){
-				ret = 2;
-			}
-			if (position + j <= 30 && position + j > 20){
-				ret = 3;
-			}
-			if (position + j <= 40 && position + j > 30){
-				ret = 4;
-			}
-		}
-	}
+    public void move(String name, int diceroll) {
+        GamePiece temp = null;
+        int position = 0;
+
+        for (int i = 0; i < 40; i++) {
+            if (gameboardArray[i] != null && name.equals(gameboardArray[i].getName())) {
+                position = i;
+                temp = gameboardArray[i];
+                gameboardArray[i] = null;
+            }
+        }
+        if (position + diceroll < 40) {
+            if (gameboardArray[position + diceroll] == null) {
+                gameboardArray[position + diceroll] = temp;
+                temp = null;
+            }
+        } else {
+            if (gameboardArray[(position + diceroll) - 40] == null) {
+                gameboardArray[(position + diceroll) - 40] = temp;
+                temp = null;
+            }
+        }
+    }
+
+    public int moveUI(String name, int diceroll, int i) {
+        int oldPosition = getPosition(name) - diceroll;
+        if (oldPosition + i < 10) {
+            return 1;
+        }
+        if (oldPosition + i < 20) {
+            return 2;
+        }
+        if (oldPosition + i < 30) {
+            return 3;
+        }
+        if (oldPosition + i < 40) {
+            return 4;
+        }
+        return -1;
+    }
 }
