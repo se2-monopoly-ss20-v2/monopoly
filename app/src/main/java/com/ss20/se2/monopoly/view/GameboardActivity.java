3cd351fd9fd0e2b88f807166d08a55e73532b6d6
Chfaeser <62070587+Chfaeser@users.noreply.github.com>
2020-06-04 23:48:07 +0200

    Implementing an alternative to shaking the device
    Implemented an alternative to shaking the device to roll
    Changed some UI Elements
    Player can now roll the dice by shaking the device or pressing the "Roll Dice" button after starting their turn

--- app/src/main/java/com/ss20/se2/monopoly/view/GameboardActivity.java
+++ app/src/main/java/com/ss20/se2/monopoly/view/GameboardActivity.java
@@ -58,6 +58,7 @@ public class GameboardActivity extends AppCompatActivity implements DeedFragment
 	TextView view_position;
 	TextView view_balance;
 	TextView updateBalance;
+	Button altbutton;
 
 	Dice dice = new Dice();
 	Dice dice2 = new Dice();
@@ -151,25 +152,22 @@ public class GameboardActivity extends AppCompatActivity implements DeedFragment
 		view_position = findViewById(R.id.number_playerposition);
 		view_balance = findViewById(R.id.text_balance);
 		updateBalance = findViewById(R.id.changeOfBalance);
+		altbutton = findViewById(R.id.altbutton);
 
 
 		button_rollDice.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 
-				Toast.makeText(getApplicationContext(), "Shake the device to roll!", Toast.LENGTH_SHORT).show();
-
+				Toast.makeText(getApplicationContext(), "Shake or tap to roll!", Toast.LENGTH_SHORT).show();
 
 				mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 				accel = 9f;
 				currentaccel = SensorManager.GRAVITY_EARTH;
 				lastaccel = SensorManager.GRAVITY_EARTH;
-
-
-
+				altbutton.setVisibility(View.VISIBLE);
 
 				final SensorEventListener mSensorListener = new SensorEventListener(){
-
 
 					@Override
 					public void onSensorChanged(SensorEvent event){
@@ -186,6 +184,8 @@ public class GameboardActivity extends AppCompatActivity implements DeedFragment
 						if(accel > 11){
 
 							mSensorManager.unregisterListener(this);
+							altbutton.setVisibility(View.GONE);
+
 							roll1 = dice.roll();
 							roll2 = dice2.roll();
 							amount = roll1 + roll2;
@@ -219,6 +219,42 @@ public class GameboardActivity extends AppCompatActivity implements DeedFragment
 						//Method not needed but required for the sensor to work
 					}
 				};
+
+				//Alternative to shaking the device
+				altbutton.setOnClickListener(new View.OnClickListener(){
+					@Override
+					public void onClick(View v){
+
+						mSensorManager.unregisterListener(mSensorListener);
+						altbutton.setVisibility(View.GONE);
+
+						roll1 = dice.roll();
+						roll2 = dice2.roll();
+						amount = roll1 + roll2;
+
+						checkDouble(roll1, roll2);
+						if (checkDouble(roll1, roll2)){
+							doublescounter++;
+						}else{
+							doublescounter = 0;
+						}
+						if (doublescounter == 3){
+							//Move to jail
+						}
+						setOldBalance(currentPlayer.getBalance());
+						gameboard.move("Player 1", amount);
+
+						updateBalance.setText("");
+						currentPlayer.setCurrentPosition(gameboard.getPosition("Player 1"));
+						checkPlayersPosition(currentPlayer);
+
+						view_numberDice.setText("Roll 1: " + Integer.toString(roll1));
+						view_numberDice2.setText("Roll 2: " + Integer.toString(roll2));
+						view_position.setText(Integer.toString(gameboard.getPosition("Player 1")));
+						findViewById(R.id.playericon).setX(fields[gameboard.getPosition("Player 1")].getX());
+						findViewById(R.id.playericon).setY(fields[gameboard.getPosition("Player 1")].getY());
+					}
+				});
 
 				Objects.requireNonNull(mSensorManager).registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
 						SensorManager.SENSOR_DELAY_GAME);
